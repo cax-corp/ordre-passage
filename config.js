1fef3cd61a4c2457d7b28c980469c019d0df1b0e
Thomas BARIAL <231519639+cax-corp@users.noreply.github.com>
2026-05-19 17:02:54 +0200

    Update supabaseAnonKey in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://yelbjsenvcncxnigkdxc.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllbGJqc2VudmNuY3huaWdrZHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTE5NTYsImV4cCI6MjA5NDc2Nzk1Nn0.1ppYJD4OWOYLx4pjM012oSG5H3a492OHFsDsZqJSUs0"
+  supabaseAnonKey: "sb_publishable_nruAZcB7nAyl8Gy5HIhrEg_Q3GPMYSe"
 };
